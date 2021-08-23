311e41207a407a60bd0b7619a5baa9343d34b2d4
Ashley Slaney <ashleyslaney96@gmail.com>
2021-08-23 16:12:38 +0100

    refactor logic

--- src/main/java/Fizzbuzz.java
+++ src/main/java/Fizzbuzz.java
@@ -1,19 +1,18 @@
 public class Fizzbuzz {
     public String generate(Integer upto) {
-        String output = "";
-        for (short i = 1; i <= upto; i++) {
-            if (i < 2) {
-                output += "" + i;
-            } else if (i % 15 == 0) {
-                output += ", " + "FizzBuzz";
-            } else if (i % 3 == 0) {
-                output += ", " + "Fizz";
-            } else if (i % 5 == 0) {
-                output += ", " + "Buzz";
+        String[] fizzbuzz = new String[upto];
+
+        for (short i = 0; i < upto; i++) {
+            if ((i + 1) % 15 == 0) {
+                fizzbuzz[i] = "FizzBuzz";
+            } else if ((i + 1) % 3 == 0) {
+                fizzbuzz[i] = "Fizz";
+            } else if ((i + 1) % 5 == 0) {
+                fizzbuzz[i] = "Buzz";
             } else {
-                output += ", " + i;
+                fizzbuzz[i] = Integer.toString(i + 1);
             }
         }
-        return output;
+        return String.join(", ", fizzbuzz);
     }
 }
