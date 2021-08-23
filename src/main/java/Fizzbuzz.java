043d5dd907a73c0e926dec9e6a8cde680ec6ddff
Ashley Slaney <ashleyslaney96@gmail.com>
2021-08-23 15:22:48 +0100

    passes when given 2

--- src/main/java/Fizzbuzz.java
+++ src/main/java/Fizzbuzz.java
@@ -1,5 +1,13 @@
 public class Fizzbuzz {
     public String generate(Integer upto) {
-        return "1";
+        String output = "";
+        for (short i = 1; i <= upto; i++) {
+            if (i < 2) {
+                output += "1";
+            } else {
+                output += ", " + i;
+            }
+        }
+        return output;
     }
 }
